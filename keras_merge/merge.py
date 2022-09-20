710652dd99f515f40e4c59c895be183f99fd1ffc
Nicolas Weber <nicolas.weber@neclab.eu>
2022-09-20 08:16:17 +0200

    initial commit

---
+++ keras_merge/merge.py
@@ -0,0 +1,60 @@
+__all__ = ['merge']
+
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Layer, InputLayer, Concatenate
+from typing import Optional, List, Dict, Tuple, Union, Any
+from keras.engine.keras_tensor import KerasTensor
+from keras.utils.object_identity import Reference
+
+def merge(
+		A: Model,
+		B: Model,
+		inputs: Union[List[KerasTensor], Tuple[KerasTensor], Dict[str, KerasTensor], KerasTensor],
+		outputs: Union[tuple, list, dict, KerasTensor],
+		mapping: Dict[Reference, Reference],
+		**kwargs: Optional[Dict[str, Any]]) -> Model:
+	def clone(layer: Layer) -> Layer:
+		assert isinstance(layer, Layer)
+		return layer.__class__.from_config(layer.get_config())
+
+	refs = {}
+	def deref(nodes: Union[tuple, list, dict, KerasTensor]):
+		if		isinstance(nodes, tuple):	return tuple(deref(n) for n in nodes)
+		elif	isinstance(nodes, list):	return list (deref(n) for n in nodes)
+		elif	isinstance(nodes, dict):	return {k: deref(v) for k, v in nodes.items()}
+		elif	isinstance(nodes, KerasTensor):
+			x = refs.get(nodes.ref())
+			assert x is not None, f'missing ref: {nodes}'
+			return x
+		assert False, f'unsupported type: {nodes}'
+
+	def clone_model(M: Model) -> None:
+		assert isinstance(M, Model)
+		for l in M.layers:
+			if l.output.ref() not in refs:
+				node	= l.inbound_nodes[0]; assert node.layer is l
+				cl		= clone(l)
+				oref	= l.output.ref()
+
+				if isinstance(l, InputLayer):
+					refs[oref] = cl.output
+				else:
+					cinputs = deref(node.keras_inputs)
+					if isinstance(l, Concatenate):	refs[oref] = cl( cinputs)
+					else:							refs[oref] = cl(*cinputs)
+
+	clone_model(A)
+
+	assert isinstance(mapping, dict)
+	for k, v in mapping.items():
+		assert isinstance(k, Reference)
+		assert isinstance(v, Reference)
+		refs[k] = refs.get(v)
+
+	clone_model(B)
+
+	new_inputs  = deref(inputs)
+	new_outputs = deref(outputs)
+
+	assert kwargs is None or isinstance(kwargs, dict)
+	return Model(inputs=new_inputs, outputs=new_outputs, **kwargs)
---
+++ tests/test0.py
@@ -0,0 +1,31 @@
+import keras_merge as km
+from tensorflow.keras import Model, Input
+import numpy as np
+from keras.utils import plot_model
+
+def model(mul):
+    x = Input(shape=(2, 3))
+    y = Input(shape=(2, 3))
+    if mul: z = x * y
+    else:   z = x + y
+    return Model(inputs=[x, y], outputs=z)
+
+A = model(True)
+B = model(False)
+
+C = km.merge(A, B, [*A.inputs, B.inputs[0]], B.outputs, {B.inputs[1].ref(): A.outputs[0].ref()})
+
+a = np.random.rand(1, 2, 3)
+b = np.random.rand(1, 2, 3)
+c = np.random.rand(1, 2, 3)
+
+d = A([a, b])
+e = B([c, d])
+f = C([a, b, c])
+
+print("A+B:    ", np.reshape(e, -1))
+print("Merged: ", np.reshape(f, -1))
+
+plot_model(A, to_file='A.png', show_shapes=True)
+plot_model(B, to_file='B.png', show_shapes=True)
+plot_model(C, to_file='C.png', show_shapes=True)
